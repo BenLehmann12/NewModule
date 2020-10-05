cb1d9915e0e71d3d755a1ebe1e69a7cf39c4698d
Ben Lehmann <62218426+BenLehmann12@users.noreply.github.com>
2020-10-05 14:29:56 -0500

    Update test_input_in_functions.py

--- test_input_in_functions.py
+++ test_input_in_functions.py
@@ -4,9 +4,11 @@ from more_functions import validate_input_in_functions as validate
 
 class MyTestCase(unittest.TestCase):
     def test_good(self):
-        self.assertEqual(validate.test_score("Chem",0),validate.test_score("Chem",0))
+        self.assertEqual(validate.test_score("Chem",75),validate.test_score("Chem",75))
     def test_below(self):
         self.assertEqual(validate.test_score("Chem",-1),validate.test_score("Chem",-1))
+    def test_score_input_name(self):
+        self.assertEqual(validate.test_score("Math"),validate.test_score("Math"))
     def test_above(self):
         self.assertEqual(validate.test_score("Chem", 104), validate.test_score("Chem", 104))
     def test_invalid_message(self):
